632080f96b9b7b1e2e07b90ab293ddcda1745ce4
Ivan <ivanrosaleset24@gmail.com>
2024-05-09 13:25:28 -0300

    new proyects for portfolio

--- src/data/Data.js
+++ src/data/Data.js
@@ -8,6 +8,19 @@ import templateMobile from "../assets/templatemobile.png";
 import dsfMobile from "../assets/dsfmobile.png";
 import yummyMobile from "../assets/yummymobile.png";
 import cfsMobile from "../assets/cfsmobile.png";
+import martu from "../assets/martud.png";
+import martuMobile from "../assets/martumobile.png";
+import beyond from "../assets/beyond.png";
+import beyondMobile from "../assets/beyondmobile.png";
+import melatol from "../assets/melatold.png";
+import melatolMobile from "../assets/melatolmobile.png";
+import marwick from "../assets/marwickd.png";
+import marwickMobile from "../assets/marwickmobile.png";
+import masongray from "../assets/masongrayd.png";
+import masonMobile from "../assets/masonmobile.png";
+
+
+
 
 
 const proyectos = [
@@ -50,6 +63,46 @@ const proyectos = [
       descripcion: 'Project dedicated to a U.S. fleet vehicle services company. The website was developed in WordPress, featuring over 12 internal pages, including a Blog and a dedicated page for hiring employees, with job listings and the ability to apply for them. Elementor PRO was used as a plugin for the layout, in conjunction with Crocoblock plugins to provide additional functionality.',
       imagen: cfsImg,
       imagenMobile: cfsMobile
+    },
+    {
+      nombre: 'Martudiazart',
+      id: 6,
+      url: 'https://www.martudiazart.com/',
+      descripcion: 'Project for a tattoo artist. Website includes eCommerce with WooCommerce for purchasing tattoo kits. Integrated MercadoPago for secure transactions. Internal course platform available for learning tattoo techniques. Inspiring art and education hub for tattoo enthusiasts.',
+      imagen: martu,
+      imagenMobile: martuMobile
+    },
+    {
+      nombre: 'Beyond Agency',
+      id: 7,
+      url: 'https://www.beyondagency.ai/',
+      descripcion: 'Project for Beyond Agency, where expertise in data, media, creativity, and AI drives business results. The website showcases their integrated approach to marketing, highlighting how they harness cutting-edge technologies to deliver impactful solutions for clients.',
+      imagen: beyond,
+      imagenMobile: beyondMobile
+    },
+    {
+      nombre: 'Melatol',
+      id: 8,
+      url: 'https://www.melatol.com.ar/',
+      descripcion: 'Project for Melatol, an institutional website for a product by Gramon Pharmaceuticals. Melatol offers a solution for insomnia, promoting better sleep and rest. The website provides information about the product, its benefits, and how it can improve sleep quality, helping visitors find relief and achieve a restful night.',
+      imagen: melatol,
+      imagenMobile: melatolMobile
+    },
+    {
+      nombre: 'Marwick',
+      id: 9,
+      url: 'https://www.marwick.com.ar/',
+      descripcion: 'Project for Marwick, a tea marketing and production company. The website serves as their institutional hub, providing information about their brand and products. It showcases their commitment to quality and sustainability in the tea industry, offering visitors an immersive experience into the world of Marwick teas.',
+      imagen: marwick,
+      imagenMobile: marwickMobile
+    },
+    {
+      nombre: 'Mason Gray',
+      id: 10,
+      url: 'https://www.masongray.nyc/',
+      descripcion: 'Project for Mason Gray, a luxury apartment marketing company based in the USA. The website serves as a showcase for their exquisite properties, highlighting the allure and sophistication of each residence..',
+      imagen: masongray,
+      imagenMobile: masonMobile
     },
     
   ]
